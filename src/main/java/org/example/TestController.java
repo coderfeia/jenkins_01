0ebd970cf3417cc24fbbeb8538d8558457524e43
coderfeia <51443311+coderfeia@users.noreply.github.com>
2026-04-07 21:04:35 +0800

    Update TestController.java
    添加注释

--- src/main/java/org/example/TestController.java
+++ src/main/java/org/example/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
+    // 加点注释
     @RequestMapping("/test")
     public String test(){
         return "hello world";
